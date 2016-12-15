5fb77645d1ae63a490a365af866fe8eb2fe2b14e
nursultan156 <nursultan156@gmail.com>
2016-12-16 00:45:54 +0600

    code formatting

--- dist/repository.js
+++ dist/repository.js
@@ -5,62 +5,62 @@ var connector = require('./connector');
 var repository_core = require('./repository_core');
 var count = 0;
 
-var repository = function(url, options){
+var repository = function (url, options) {
 
     var _connector = connector.new(url, options);
 
     this._id = count;
 
-    this.createOne = function(payload, callback){
+    this.createOne = function (payload, callback) {
         var inst = new repository_core(_connector);
         inst.createOne(payload, callback);
     };
-    this.readOne = function(payload, callback){
+    this.readOne = function (payload, callback) {
         var inst = new repository_core(_connector);
         inst.readOne(payload, callback);
     };
-    this.updateOne = function(payload, callback){
+    this.updateOne = function (payload, callback) {
         var inst = new repository_core(_connector);
         inst.updateOne(payload, callback);
     };
-    this.deleteOne = function(payload, callback){
+    this.deleteOne = function (payload, callback) {
         var inst = new repository_core(_connector);
         inst.deleteOne(payload, callback);
     };
 
-    this.createMany = function(payload, callback){
+    this.createMany = function (payload, callback) {
         var inst = new repository_core(_connector);
         inst.createMany(payload, callback);
     };
-    this.readMany = function(payload, callback){
+    this.readMany = function (payload, callback) {
         var inst = new repository_core(_connector);
         inst.readMany(payload, callback);
     };
-    this.updateMany = function(payload, callback){
+    this.updateMany = function (payload, callback) {
         var inst = new repository_core(_connector);
         inst.updateMany(payload, callback);
     };
-    this.deleteMany = function(payload, callback){
+    this.deleteMany = function (payload, callback) {
         var inst = new repository_core(_connector);
         inst.deleteMany(payload, callback);
     };
 
-    this.aggregate = function(payload, callback){
+    this.aggregate = function (payload, callback) {
         var inst = new repository_core(_connector);
         inst.aggregate(payload, callback);
     };
-    this.count = function(payload, callback){
+    this.count = function (payload, callback) {
         var inst = new repository_core(_connector);
         inst.count(payload, callback);
     };
-    this.createIndex = function(payload, callback){
+    this.createIndex = function (payload, callback) {
         var inst = new repository_core(_connector);
         inst.createIndex(payload, callback);
     };
 
 };
 
-module.exports.new = function(url, options){
+module.exports.new = function (url, options) {
     count++;
     return new repository(url, options);
 
--- test/test.js
+++ test/test.js
@@ -15,24 +15,26 @@ var d2_1 = null;
 var d2_2 = null;
 
 async.series([
-        function(callback){
+        function (callback) {
             d1_1 = new Date();
             var count = 0;
             async.whilst(
-                function() { return count < 1000000; },
-                function(callback) {
+                function () {
+                    return count < 1000000;
+                },
+                function (callback) {
                     count++;
 
                     var payload = {
                         collectionName: 'c1',
                         document: {
-                            a:1,
-                            b:2
+                            a: 1,
+                            b: 2
                         }
                     };
 
-                    r1.createOne(payload, function(err,res){
-                        if(err) return callback(err);
+                    r1.createOne(payload, function (err, res) {
+                        if (err) return callback(err);
                         console.log(count);
                         callback(null);
                     });
@@ -40,30 +42,32 @@ async.series([
                 },
                 function (err, n) {
                     d1_2 = new Date();
-                    if(err) return callback(err);
+                    if (err) return callback(err);
                     callback(null);
                 }
             );
 
         },
-        function(callback){
+        function (callback) {
             d2_1 = new Date();
             var count = 0;
             async.whilst(
-                function() { return count < 1000000; },
-                function(callback) {
+                function () {
+                    return count < 1000000;
+                },
+                function (callback) {
                     count++;
 
                     var payload = {
                         collectionName: 'c2',
                         document: {
-                            a:1,
-                            b:2
+                            a: 1,
+                            b: 2
                         }
                     };
 
-                    r2.createOne(payload, function(err,res){
-                        if(err) return callback(err);
+                    r2.createOne(payload, function (err, res) {
+                        if (err) return callback(err);
                         console.log(count);
                         callback(null);
                     });
@@ -71,7 +75,7 @@ async.series([
                 },
                 function (err, n) {
                     d2_2 = new Date();
-                    if(err) return callback(err);
+                    if (err) return callback(err);
                     callback(null);
                 }
             );
@@ -82,8 +86,8 @@ async.series([
         console.log(err);
         console.log(res);
 
-        console.log(d1_2-d1_1);
-        console.log(d2_2-d2_1);
+        console.log(d1_2 - d1_1);
+        console.log(d2_2 - d2_1);
 
     });
 
